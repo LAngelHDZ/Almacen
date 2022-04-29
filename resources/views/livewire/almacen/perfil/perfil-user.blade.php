e5c44d36262b5abe646f8319dace18764c057ba5
Luis Angel Laptop <guitarra1231@gmail.com>
2022-04-29 15:37:47 -0500

    view profile
    vista profile

--- resources/views/livewire/almacen/perfil/perfil-user.blade.php
+++ resources/views/livewire/almacen/perfil/perfil-user.blade.php
@@ -1,21 +1,34 @@
 <div>
-    <div class="container ">
-        <div class=" p-4 mx-5 rounded border ">
-            <div class="bg-white flex">
-                <div class="w-75 border ">
-                    <p class="text-bold h6">Public Info</p>
-                    <div class="mt-2 px-5 py-1">
+    <div class="container ml-5 w-100">
+        <div class="inline-flex py-1 mr-2 bg-white rounded-sm ">
+            <div>
+                <p class="block w-full px-2 py-2 text-center ">Settings</p>
+                <nav class="w-full text-white bg-indigo-500">
+                    <p class="block px-2 py-2 text-center border-bottom hover:bg-indigo-600">Personal</p>
+                    <p class="block px-2 py-2 text-center border-bottom hover:bg-indigo-600">Email</p>
+                    <p class="block px-2 py-2 text-center border-bottom hover:bg-indigo-600">Firma</p>
+                    <p class="block px-2 py-2 text-center rounded-bottom hover:bg-indigo-600">Contraseña</p>
+                </nav>
+            </div>
+        </div>
+        <div class="inline-flex w-75">
+                <div class="flex w-full py-3 bg-white rounded-sm ">
+                    <div class="w-2/3">
+                        <p class="ml-5 text-bold h6">Informacion Personal</p>
+                    <div class="px-5 py-1 mt-2">
                         <label class="block mb-0" for="">Usuario</label>
-                        <input disabled type="text" class="form-imput rounded-sm w-full">
-                        <label class="block mt-2 mb-0" for="">Firma</label>
-                        <input type="text" class="form-imput rounded-sm w-full">
-                        <button class="mt-4 block btn-info px-3 py-2 rounded-sm"> Guardar</button>
+                        <p>Alberto Hernandez Garcia</p>
+                        <label class="block mt-2 mb-0" for="">Departamento</label>
+                        <p>Departamento de Vigilancia Sanitaria</p>
+                        <label class="block mt-2 mb-0" for="">Area</label>
+                        <p>Area de Lboratorio de Toxicologia</p>
+                        <button class="block px-3 py-2 mt-4 text-white bg-indigo-500 rounded-sm hover:bg-indigo-600"> Guardar</button>
                     </div>
                 </div>
-                <div class="ml-2 p-2 border w-25">
+                <div class="w-1/3 p-2 ml-2 border-left border-primary ">
                     IMAGEN DE PERFIL
                 </div>
-            </div>
+        </div>
         </div>
     </div>
 </div>
