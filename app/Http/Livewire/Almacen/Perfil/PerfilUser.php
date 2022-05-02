908aff73b6a12aa100038facf19c1886eadb19ff
Luis Angel Laptop <guitarra1231@gmail.com>
2022-05-02 09:44:53 -0500

    update view email profile

--- app/Http/Livewire/Almacen/Perfil/PerfilUser.php
+++ app/Http/Livewire/Almacen/Perfil/PerfilUser.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Almacen\Perfil;
 
 use App\Models\empleado;
+use App\Models\firma_digital;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -10,20 +11,26 @@ use Livewire\Component;
 class PerfilUser extends Component
 {
 
-    public $info,$idempleado;
+    public $info,$idempleado, $emailModel, $efirmaModel,$efirmaInfo, $emailInfo;
     public function mount(){
         $userid=Auth::user()->id;
        $this->info= User::join('empleados','users.id','=','empleados.id_user')
         ->join('areas','areas.id','=','empleados.area')
         ->join('departamentos as dep','dep.id','=','areas.id_departamento')
         // ->join('firma_digitals as efirma','empleados.id','=','efirma.id_empleado')
-        ->select('users.name','users.email','empleados.id_user','empleados.id as ide','empleados.clave','empleados.cargo','empleados.rfc',
+        ->select('users.name','users.email','empleados.id_user','empleados.id as ide','empleados.clave as clave_emp','empleados.cargo','empleados.rfc',
         'areas.clave','areas.area','dep.clave as clavedep','dep.departamento'
         )
         ->where('users.id',$userid)
         ->get();
 
         $this->idempleado=$this->info[0]->ide;
+        $this->emailInfo=$this->info[0]->email;
+    }
+
+    public function queryEfirma(){
+        $efirma= firma_digital::where('id_empleado',$this->idempleado);
+        $this->efirmaInfo= $efirma->firma;
     }
 
     public function render()
--- resources/views/livewire/almacen/perfil/perfil-user.blade.php
+++ resources/views/livewire/almacen/perfil/perfil-user.blade.php
@@ -6,12 +6,12 @@
                 <nav class="w-full text-white bg-indigo-500">
                     <a @click="showpersonal(personal)" class="block px-2 py-2 text-center text-white border-bottom hover:bg-indigo-600">Personal</a>
                     <a @click="showemail(email)" class="block px-2 py-2 text-center  text-white  border-bottom hover:bg-indigo-600">Email</a>
-                    <a @click="showefirma(efirma)" class="block px-2 py-2 text-center  text-white  border-bottom hover:bg-indigo-600">eFirma</a>
+                    <a @click="showefirma(efirma)" wire:click.prevent='queryEfirma' class="block px-2 py-2 text-center  text-white  border-bottom hover:bg-indigo-600">eFirma</a>
                     {{-- <p class="block px-2 py-2 text-white text-center rounded-bottom hover:bg-indigo-600">Contraseña</p> --}}
                 </nav>
             </div>
         </div>
-       
+
         <div class="inline-flex w-75 ml-5">
                 <div class="flex w-full py-3 bg-white rounded-sm ">
                   @foreach ($info as $data)
@@ -28,10 +28,12 @@
                     <div class="px-5 py-1 mt-2">
                       <h6 class="block m-0 text-bold" for="">Usuario</h6>
                       <p class="m-0 p-0">{{ $data->name }}</p>
-                      <label class="block mt-2 mb-0 text-bold" for="">Departamento</label>
-                      <p>{{ $data->departamento }}</p>
-                      <label class="block mt-2 mb-0 text-bold" for="">Area</label>
-                      <p>{{ $data->area }}</p>
+                      <h6 class="block m-0 text-bold" for="">Clave</h6>
+                      <p class="m-0 p-0">{{ $data->clave_emp }}</p>
+                      <label class="block mb-0 text-bold" for="">Departamento</label>
+                      <p>{{ $data->departamento.' / '.$data->clavedep }}</p>
+                      <label class="block  mb-0 text-bold" for="">Area</label>
+                      <p>{{ $data->area.' / '.$data->clave }}</p>
                       {{-- <button class="block px-3 py-2 mt-4 text-white bg-indigo-500 rounded-sm hover:bg-indigo-600"> Guardar</button> --}}
                     </div>
                   </div>
@@ -43,15 +45,15 @@
                     x-transition:enter-end="opacity-100 scale-100"
                     {{-- x-transition:leave="transition ease-in duration-600" --}}
                     {{-- x-transition:leave-start="opacity-100 scale-100" --}}
-                    {{-- x-transition:leave-end="opacity-0 scale-90" --}} 
+                    {{-- x-transition:leave-end="opacity-0 scale-90" --}}
                     class="w-2/3">
                         <p class="ml-5 text-bold h6">Informacion de Email</p>
                         <div class="px-5 py-1 mt-2">
                             <label class="block mb-0" for="">Correo Electronico</label>
-                            <p>Alberto@gmail.com</p>
+                            <p>{{$emailInfo}}</p>
                             <label class="block mt-2 mb-0" for="">Nuevo correo</label>
-                            <input type="text" class="form-input rounded-sm hover:border-blue-700 w-75">
-                            
+                            <input wire:model='email' type="text" class="form-input rounded-sm hover:border-blue-700 w-75">
+
                             <button class="block px-3 py-2 mt-4 text-white bg-indigo-500 rounded-sm hover:bg-indigo-600"> Guardar</button>
                         </div>
                     </div>
@@ -61,16 +63,21 @@
                     x-transition:enter-end="opacity-100 scale-100"
                     {{-- x-transition:leave="transition ease-in duration-600" --}}
                     {{-- x-transition:leave-start="opacity-100 scale-100" --}}
-                    {{-- x-transition:leave-end="opacity-0 scale-90" --}} 
+                    {{-- x-transition:leave-end="opacity-0 scale-90" --}}
                     class="w-2/3 ">
                         <p class="ml-5 text-bold h6">Informacion de e.Firma</p>
                         <div class="px-5 py-1 mt-2">
                             <label class="block mb-0" for="">e.Firma</label>
                             {{-- <p>123456789</p>
                             <label class="block mt-2 mb-0" for="">Cadena digital</label> --}}
-                            <p>iyuh5825N28SHf5JiuhgdLKiGr58hfjuo5</p>
+                            @if ($efirmaInfo!=null)
+                            <p>{{$efirmaInfo}}</p>
+                            @else
+                            <p class="text-gray-500">No existe firma</p>
+                            @endif
+
                             <label class="block mt-2 mb-0" for="">Genenerar firma</label>
-                            <input type="text" class="form-input rounded-sm hover:border-blue-700 w-75">
+                            <input wire:model='efirmaModel' type="text" class="form-input rounded-sm hover:border-blue-700 w-75">
                             <button class=" text-white px-3 py-2 bg-indigo-500 rounded-sm hover:bg-indigo-600"> Generar</button>
                             <button class="block px-3 py-2 mt-4 text-white bg-indigo-500 rounded-sm hover:bg-indigo-600"> Guardar</button>
                         </div>
