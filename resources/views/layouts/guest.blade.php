741aca158cc251710b304ec0c623acbf5d655266
LAngelHDZ <82234456+LAngelHDZ@users.noreply.github.com>
2022-06-07 22:02:13 -0500

    login update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,8 +22,11 @@
 
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <div class="font-sans text-gray-900 antialiased ">
+           
+            <div>
+                {{ $slot }}
+            </div>
         </div>
 
         @livewireScripts
--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,9 +1,12 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
+   
+            <div class="text-center mb-4 pb-4">
+                {{-- <img class="" src="{{ asset('img/SSFederal.jpeg') }}" alt=""> --}}
+            </div>
+            <div class="mt-4 pt-4">
+                {{ $logo }}
+            </div>
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
 </div>
